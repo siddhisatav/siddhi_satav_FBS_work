6c512426c66b72f6b4e6ff021bf21b4394ca869d
siddhisatav <ssatav793@gmail.com>
2025-10-06 10:11:46 +0530

    Update Q6.py

--- Core_Python/Assignments/Assignment -2/Q6.py
+++ Core_Python/Assignments/Assignment -2/Q6.py
@@ -1,5 +1,8 @@
+# WAP to calculate selling price of book based on cost price and discount.
+
 cost = int(input("enter cost price :"))
 discount = int(input("enter discount percent :"))
 discount_amount = (discount/100) * cost
 selling_price = cost - discount_amount
+
 print("selling price is : ", selling_price)
