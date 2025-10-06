07d2bb7ac814981affe4ded01a4e853332496369
siddhisatav <ssatav793@gmail.com>
2025-10-06 10:10:54 +0530

    Update Q4.py

--- Core_Python/Assignments/Assignment -2/Q4.py
+++ Core_Python/Assignments/Assignment -2/Q4.py
@@ -1,3 +1,5 @@
+# WAP to calculate area of triangle and rectangle
+
 l = int(input("enter lenght"))
 b =int(input("enter breadth"))
 
@@ -8,4 +10,5 @@ B = int(input("enter base value : "))
 H=int(input("enter height :"))
       
 Area_of_triangle = 1/2 * B * H
+
 print("Area of triangle is : ",Area_of_triangle) 
