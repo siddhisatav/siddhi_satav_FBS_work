fea80ff9f6e18f15135410cac8b0f5f8942a6666
Siddhi Santosh Satav <ssatav793@gmail.com>
2025-10-16 10:37:05 +0530

    Delete Assignment -3/Q2.py

--- Assignment -3/Q2.py
+++
@@ -1,9 +0,0 @@
-# 2. Write a program to input any alphabet and check whether it is vowel or consonant.
-v = ["a","e","i","o","u","A","E","I","O","U"]
-
-character = input("enter alphabet : ")
-if character in v :
-    print("character is Vowel")
-    
-else : 
-    print("character is Consonant")
